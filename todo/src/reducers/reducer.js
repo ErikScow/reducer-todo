2e0f62da17354fc19f8352587b9f5897901182fa
Erik Scow <erikscow@gmail.com>
2020-04-08 18:08:14 -0700

    Ran CRA. Added reducer file within new folder.

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,18 @@
+const reducer = (state, action) => {
+    switch (action.type){
+
+        default:
+            return state
+    }
+}
+
+//initial state:
+const todo = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }
+]
+
+export {reducer, todo}
